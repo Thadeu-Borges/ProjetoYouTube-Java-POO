afd9df7b9643e789b52af215d544c8f254f3c838
Thadeu <123416610+Thadeu-Borges@users.noreply.github.com>
2023-02-19 05:04:43 -0300

    Java POO

---
+++ src/projetoyoutube/AcoesVideo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package projetoyoutube;
+
+/**
+ *
+ * @author AAAA
+ */
+public interface AcoesVideo {
+
+    public void play();
+
+    public void pause();
+
+    public void like();
+
+}
